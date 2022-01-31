a2b154ba6f44098072a98c9d67d57f5ae0a85f9c
Наталья <94108495+Nataly001@users.noreply.github.com>
2022-01-31 16:52:14 +0200

    Home page

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const card = document.getElementsByClassName("card");
+const slider = document.getElementById("slider");
+const buttonRight = document.getElementById("slide-right");
+const buttonLeft = document.getElementById("slide-left");
+
+buttonLeft.addEventListener("click", () =>{
+  slider.scrollLeft -= slider.clientWidth/3;
+});
+
+buttonRight.addEventListener("click", () =>{
+  slider.scrollLeft += slider.clientWidth/3;
+});
+
+const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+///alert(maxScrollLeft);
+
+//Autoplay slider
+function autoPlay(){
+  if (slider.scrollLeft > (maxScrollLeft - 1)){
+    slider.scrollLeft -= maxScrollLeft;
+  }
+  else{
+    slider.scrollLeft += 1;
+  }
+}
+let play = setInterval(autoPlay, 50);
+
+//Pause the slider on hover
+for (let i = 0; i < card.length; i++){
+  card[i].addEventListener("mouseover", () => {
+    clearInterval(play);
+  })
+  card[i].addEventListener("mouseout", () => {
+    return play = setInterval(autoPlay, 50);
+  })
+}
